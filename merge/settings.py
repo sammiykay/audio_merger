a23c2a60151eedbcdb4aaa5b31b83009f7cec07d
sammiykay <kayodeola47@gmail.com>
2025-05-14 23:59:27 +0100

    new

--- merge/settings.py
+++ merge/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(138s8fw7-ivg5+5qbwvgaetx79=%ashykx$i(c9zr^p$u+rg_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
